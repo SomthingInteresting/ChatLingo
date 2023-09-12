74f46586a2591e72d17dd5d012f86157f979aa0f
Quddus Rahman <qrahman115@gmail.com>
2023-09-12 16:52:05 +0100

    added firebase login component

---
+++ frontend/src/components/LoginComponent.js
@@ -0,0 +1,49 @@
+// LoginComponent.js
+import React, { useState } from 'react';
+import { signIn } from '../utils/authService';
+import '../styles/LoginStyles.css';
+
+
+const LoginComponent = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+
+  const handleLogin = async () => {
+    try {
+      await signIn(email, password);
+      // Handle successful login, e.g., redirect to another page
+    } catch (err) {
+      // Handle error during login
+      setError(err.message);
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      {error && <p className="error-message">{error}</p>}
+      <div className="input-group">
+        <label>Email:</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Enter your email"
+        />
+      </div>
+      <div className="input-group">
+        <label>Password:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Enter your password"
+        />
+      </div>
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default LoginComponent;
---
+++ frontend/src/utils/authService.js
@@ -0,0 +1,47 @@
+import { auth } from '../firebase';
+import { 
+  createUserWithEmailAndPassword, 
+  signInWithEmailAndPassword, 
+  signOut as firebaseSignOut, 
+  onAuthStateChanged 
+} from "firebase/auth";
+
+export const signUp = async (email, password) => {
+  try {
+    await createUserWithEmailAndPassword(auth, email, password);
+    // User signed up successfully
+  } catch (error) {
+    // Handle error (e.g., user already exists)
+    console.error("Error signing up:", error);
+  }
+};
+
+export const signIn = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    // User signed in successfully
+  } catch (error) {
+    // Handle error (e.g., wrong password)
+    console.error("Error signing in:", error);
+  }
+};
+
+export const signOut = async () => {
+  try {
+    await firebaseSignOut(auth);
+    // User signed out successfully
+  } catch (error) {
+    // Handle error
+    console.error("Error signing out:", error);
+  }
+};
+
+export const observeAuthState = (onUserAuthenticated, onUserLoggedOut) => {
+  return onAuthStateChanged(auth, (user) => {
+    if (user) {
+      onUserAuthenticated(user);
+    } else {
+      onUserLoggedOut();
+    }
+  });
+};
