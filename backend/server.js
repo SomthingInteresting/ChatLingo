b6fb548eaf342ac1d2361a97e605d6b88620f944
Quddus Rahman <qrahman115@gmail.com>
2023-10-04 10:34:35 +0100

    added basic implementation of backend with frontend communication

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",  // Your frontend server
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(3001, () => {
+  console.log("listening on *:3001");
+});
