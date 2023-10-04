431e362e7e59279d6a805fd16adb6b7267652024
Quddus Rahman <qrahman115@gmail.com>
2023-10-04 16:05:28 +0100

    added a logot button to chat

--- frontend/src/components/ChatComponent.js
+++ frontend/src/components/ChatComponent.js
@@ -1,18 +1,29 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { signOut } from '../utils/authService';
 import { io } from 'socket.io-client';
 import { apiUrl } from '../config';
 
-// const socket = io('https://chat-lingo-backend.onrender.com');
 const socket = io(apiUrl);
 
 const Chat = () => {
   const [message, setMessage] = useState('');
   const [chat, setChat] = useState([]);
+  const navigate = useNavigate();
 
   const sendMessage = (e) => {
     e.preventDefault();
     socket.emit('chat message', message);
     setMessage('');
+  };
+
+  const handleLogout = async () => {
+    try {
+      await signOut();
+      navigate('/');
+    } catch (error) {
+      console.error("Error signing out:", error);
+    }
   };
 
   useEffect(() => {
@@ -28,6 +39,7 @@ const Chat = () => {
 
   return (
     <div>
+      <button onClick={handleLogout}>Logout</button> {/* Adding logout button */}
       <ul id="messages">
         {chat.map((msg, idx) => (
           <li key={idx}>{msg}</li>
